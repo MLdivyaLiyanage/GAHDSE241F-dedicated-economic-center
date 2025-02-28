f4f78b70c7bcac91f0237613aed5011e97af3ca6
W.D.G. Shehan Harsha Kumara <159415858+ShehanHarshaKumara@users.noreply.github.com>
2025-02-28 19:34:32 +0530

    login & Register

--- final-project-website/src/main.jsx
+++ final-project-website/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Firstpage from './FirstPage/Firstpage.jsx';
+import SingPage from './LoginPage/SingPage.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Firstpage />
+    <SingPage/>
   </React.StrictMode>
 );
