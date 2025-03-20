4b970508e0d7d98a3140ac7a1efd562b60be6916
sadun kumara <dilmiravihansa2002.1.14@gmail.com>
2025-03-20 12:25:13 +0530

    Combine Pages

--- final-project-website/src/main.jsx
+++ final-project-website/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import CategaryPageFarmer from "./CategaryPageFarmer/CategaryPageFarmer";
+import CombineFoods from "./Routes/CombineFoods";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  {<CategaryPageFarmer/>}
+    <CombineFoods />
   </React.StrictMode>
 );
