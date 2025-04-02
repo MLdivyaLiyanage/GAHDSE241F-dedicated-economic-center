11b7ca571070bab72650b2a910bbdce4ac3ce73d
sadun kumara <dilmiravihansa2002.1.14@gmail.com>
2025-04-02 17:29:58 +0530

    BuyCardAndPayment pages for other foods

--- final-project-website/src/main.jsx
+++ final-project-website/src/main.jsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 // import FarmerCard from "./FarmerCard/FarmerCard.jsx";
 // import CombineHomeNavbar from "./Routes/CombineHomeNavbar.jsx";
 //import CombineFarmers from "./Routes/CombineFarmers.jsx";
-import CombineFoods from "./Routes/CombineFoods.jsx";
+//import CombineFoods from "./Routes/CombineFoods.jsx";
 // import FarmerProfile from "./UserProfiles/FarmerProfile.jsx";
 // import Profile01 from "./FarmerProfils/Profile01.jsx";
 // import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment.jsx";
@@ -19,6 +19,14 @@ import CombineFoods from "./Routes/CombineFoods.jsx";
 // import LoginPage from "./LoginPage/LoginPage.jsx";
 //import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment.jsx";
 //import genraete from "./profilegenrate/genraete.jsx";
+//import BuyCardAndPayment2 from "./PaymentPage/BuyCardAndPayment2"
+//import BuyCardAndPayment3 from "./PaymentPage/BuyCardAndPayment3"
+//import BuyCardAndPayment4 from "./PaymentPage/BuyCardAndPayment4"
+//import BuyCardAndPayment5 from "./PaymentPage/BuyCardAndPayment5"
+//import BuyCardAndPayment6 from "./PaymentPage/BuyCardAndPayment6"
+//import BuyCardAndPayment7 from "./PaymentPage/BuyCardAndPayment7"
+//import BuyCardAndPayment8 from "./PaymentPage/BuyCardAndPayment8"
+import BuyCardAndPayment9 from "./PaymentPage/BuyCardAndPayment9"
 
 
 
@@ -42,12 +50,20 @@ createRoot(document.getElementById("root")).render(
      {/* <LoginPage/>  */}
    {/*<CombineHomeNavbar/> */}
      {/* <CombineFarmers/>   */}
-  <CombineFoods/> 
+  {/* <CombineFoods/>  */}
      {/* <FarmerProfile/>  */}
      {/*<Profile01/> */}
     {/*<BuyCardAndPayment/>*/}
     {/* <FeedBack/> */}
     {/* <Massage/> */}
     {/* <genraete/> */}
+    {/* <BuyCardAndPayment2/> */}
+    {/* <BuyCardAndPayment3/> */}
+    {/* <BuyCardAndPayment4/> */}
+    {/* <BuyCardAndPayment5/> */}
+    {/* <BuyCardAndPayment6/> */}
+    {/* <BuyCardAndPayment7/> */}
+    {/* <BuyCardAndPayment8/> */}
+    <BuyCardAndPayment9/>
   </React.StrictMode>
 );
