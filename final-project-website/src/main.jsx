80cce288ed70ca0364d96a8fb7a9370649849c0d
sadun kumara <dilmiravihansa2002.1.14@gmail.com>
2025-04-03 11:00:44 +0530

    none

--- final-project-website/src/main.jsx
+++ final-project-website/src/main.jsx
@@ -1,51 +1,42 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-// import FoodSlider from "./FoodSlider/FoodSlider";
-// import FarmerSlider from "./FarmerSlider/FarmerSlider";
-// import FarmerCard from "./FarmerCard/FarmerCard.jsx";
-// import CombineHomeNavbar from "./Routes/CombineHomeNavbar.jsx";
-//import CombineFarmers from "./Routes/CombineFarmers.jsx";
-// import CombineFoods from "./Routes/CombineFoods.jsx";
-// import FarmerProfile from "./UserProfiles/FarmerProfile.jsx";
-// import Profile01 from "./FarmerProfils/Profile01.jsx";
-// import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment.jsx";
-// import LoginPage from "./LoginPage/LoginPage.jsx";
-// import FeedBack from "./RivevAndFeedBack/FeedBack.jsx";
-// import Massage from "./MassagePage/Massage.jsx";
-// import FarmerCard from "./FarmerCard/FarmerCard.jsx";
-//  import ProductCard01 from "./FarmerProductCards/ProductCard01.jsx";
-//  import Profile01 from "./FarmerProfils/Profile01.jsx";
-// import FoodCard from "./FoodCard/FoodCard.jsx";
-// import LoginPage from "./LoginPage/LoginPage.jsx";
-//import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment.jsx";
+import { createRoot } from "react-dom/client"; // Ensure this import is present
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar/NavBar";  
+import HomeFirstPage from "./Home/HomeFirstPage";
+import CombineFarmers from "./Routes/CombineFarmers";  
+import CombineFoods from "./Routes/CombineFoods";
+import FarmerProfile from "./UserProfiles/FarmerProfile"
+import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment"
+import BuyCardAndPayment2 from "./PaymentPage/BuyCardAndPayment2"
+import BuyCardAndPayment3 from "./PaymentPage/BuyCardAndPayment3"
+import BuyCardAndPayment4 from "./PaymentPage/BuyCardAndPayment4"
+import BuyCardAndPayment5 from "./PaymentPage/BuyCardAndPayment5"
+import BuyCardAndPayment6 from "./PaymentPage/BuyCardAndPayment6"
+import BuyCardAndPayment7 from "./PaymentPage/BuyCardAndPayment7"
+import BuyCardAndPayment8 from "./PaymentPage/BuyCardAndPayment8"
+import BuyCardAndPayment9 from "./PaymentPage/BuyCardAndPayment9"
 
-
-
-
-
-
-
-createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomeFirstPage />} />
+        <Route path="/farmer" element={<CombineFarmers />} />
+        <Route path="/product" element={<CombineFoods />} />
+        <Route path="/profile" element={<FarmerProfile />}/>
+        <Route path="/bellpepper" element={<BuyCardAndPayment/>}/>
+        <Route path="/cucumber" element={<BuyCardAndPayment2/>}/>
+        <Route path="/amandine-potato" element={<BuyCardAndPayment3/>}/>
+        <Route path="/carrot" element={<BuyCardAndPayment4/>}/>
+        <Route path="/pineapple" element={<BuyCardAndPayment5/>}/>
+        <Route path="/butterhead-lettuce" element={<BuyCardAndPayment6/>}/>
+        <Route path="/cauliflower" element={<BuyCardAndPayment7/>}/>
+        <Route path="/beetroot" element={<BuyCardAndPayment8/>}/>
+        <Route path="/savoy-cabbage" element={<BuyCardAndPayment9/>}/>
         
-      {/* <BuyCardAndPayment/> */}
-      {/* <LoginPage/> */}
-    {/* <FoodSlider/> */}
-    {/* <FoodCard/> */}
-   {/* <FarmerSlider/> */}
-    {/* <FarmerCard/> */}
-     {/* <Profile01/>  */}
-    {/* <ProductCard01/>  */}
-    {/* <FarmerCard/> */}
-    {/* <LoginPage/> */}
-   {/*<CombineHomeNavbar/> */}
-    {/*<CombineFarmers/> */}
-  {/*<CombineFoods/>*/}
-     { /*<FarmerProfile/> */} 
-     {/*<Profile01/> */}
-    {/*<BuyCardAndPayment/>*/}
-    {/* <FeedBack/> */}
-    {/* <Massage/> */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
