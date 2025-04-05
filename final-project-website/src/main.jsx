98e0580f3c9b4983de7f75e238cda7bbe4c35b2b
W.D.G. Shehan Harsha Kumara <159415858+ShehanHarshaKumara@users.noreply.github.com>
2025-04-05 20:43:10 +0530

    Location server

--- final-project-website/src/main.jsx
+++ final-project-website/src/main.jsx
@@ -1,166 +1,70 @@
-import  "react";
-import PropTypes from 'prop-types';
+import React from "react";
 import { createRoot } from "react-dom/client";
-import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./NavBar/NavBar";
+// import FoodSlider from "./FoodSlider/FoodSlider";
+// import FarmerSlider from "./FarmerSlider/FarmerSlider";
+// import FarmerCard from "./FarmerCard/FarmerCard.jsx";
+// import CombineHomeNavbar from "./Routes/CombineHomeNavbar.jsx";
+//import CombineFarmers from "./Routes/CombineFarmers.jsx";
+//import CombineFoods from "./Routes/CombineFoods.jsx";
+// import FarmerProfile from "./UserProfiles/FarmerProfile.jsx";
+// import Profile01 from "./FarmerProfils/Profile01.jsx";
+// import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment.jsx";
+ //import LoginPage from "./LoginPage/LoginPage.jsx";
+// import FeedBack from "./RivevAndFeedBack/FeedBack.jsx";
+// import Massage from "./MassagePage/Massage.jsx";
+// import FarmerCard from "./FarmerCard/FarmerCard.jsx";
+//  import ProductCard01 from "./FarmerProductCards/ProductCard01.jsx";
+//  import Profile01 from "./FarmerProfils/Profile01.jsx";
+// import FoodCard from "./FoodCard/FoodCard.jsx";
+// import LoginPage from "./LoginPage/LoginPage.jsx";
+//import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment.jsx";
+//import genraete from "./profilegenrate/genraete.jsx";
+//import BuyCardAndPayment2 from "./PaymentPage/BuyCardAndPayment2"
+//import BuyCardAndPayment3 from "./PaymentPage/BuyCardAndPayment3"
+//import BuyCardAndPayment4 from "./PaymentPage/BuyCardAndPayment4"
+//import BuyCardAndPayment5 from "./PaymentPage/BuyCardAndPayment5"
+//import BuyCardAndPayment6 from "./PaymentPage/BuyCardAndPayment6"
+//import BuyCardAndPayment7 from "./PaymentPage/BuyCardAndPayment7"
+//import BuyCardAndPayment8 from "./PaymentPage/BuyCardAndPayment8"
+// import BuyCardAndPayment9 from "./PaymentPage/BuyCardAndPayment9"
+import Location from "./LocationPage/Location.jsx";
 
-import HomeFirstPage from "./Home/HomeFirstPage";
-import CombineFarmers from "./Routes/CombineFarmers";
-import CombineFoods from "./Routes/CombineFoods";
-import FarmerProfile from "./UserProfiles/FarmerProfile";
-import BuyCardAndPayment from "./PaymentPage/BuyCardAndPayment";
-import BuyCardAndPayment2 from "./PaymentPage/BuyCardAndPayment2";
-import BuyCardAndPayment3 from "./PaymentPage/BuyCardAndPayment3";
-import BuyCardAndPayment4 from "./PaymentPage/BuyCardAndPayment4";
-import BuyCardAndPayment5 from "./PaymentPage/BuyCardAndPayment5";
-import BuyCardAndPayment6 from "./PaymentPage/BuyCardAndPayment6";
-import BuyCardAndPayment7 from "./PaymentPage/BuyCardAndPayment7";
-import BuyCardAndPayment8 from "./PaymentPage/BuyCardAndPayment8";
-import BuyCardAndPayment9 from "./PaymentPage/BuyCardAndPayment9";
-import LoginPage from "./LoginPage/LoginPage";
-import ProductFeedback from "./ProductReviewRating/ProductFeedback"
 
-// Layout component that conditionally renders NavBar
-const Layout = ({ children, showNavBar }) => (
-  <>
-    {showNavBar && <NavBar />}
-    {children}
-  </>
-);
-};
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-  showNavBar: PropTypes.bool.isRequired,
-};
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <Routes>
-      Login page route without NavBar
-      <Route 
-        path="/" 
-        element={
-          <Layout showNavBar={false}>
-            <LoginPage />
-          </Layout>
-        } 
-      />
-      
-      {/* All other routes with NavBar */}
-      <Route 
-        path="/home" 
-        element={
-          <Layout showNavBar={true}>
-            <HomeFirstPage />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/farmer" 
-        element={
-          <Layout showNavBar={true}>
-            <CombineFarmers />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/product" 
-        element={
-          <Layout showNavBar={true}>
-            <CombineFoods />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/profile" 
-        element={
-          <Layout showNavBar={true}>
-            <FarmerProfile />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/bellpepper" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/cucumber" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment2 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/amandine-potato" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment3 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/carrot" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment4 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/pineapple" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment5 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/butterhead-lettuce" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment6 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/cauliflower" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment7 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/beetroot" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment8 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/savoy-cabbage" 
-        element={
-          <Layout showNavBar={true}>
-            <BuyCardAndPayment9 />
-          </Layout>
-        } 
-      />
-      <Route 
-        path="/feedback" 
-        element={
-          <Layout showNavBar={false}>
-            <ProductFeedback />
-          </Layout>
-        } 
-      />
-    </Routes>
-  </Router>
+
+
+
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+
+        <Location/>
+      {/* <BuyCardAndPayment/> */}
+      {/* <LoginPage/> */}
+    {/* <FoodSlider/> */}
+    {/* <FoodCard/> */}
+   {/* <FarmerSlider/> */}
+    {/* <FarmerCard/> */}
+      {/* <Profile01/>   */}
+    {/* <ProductCard01/>  */}
+    {/* <FarmerCard/> */}
+     {/* <LoginPage/>  */}
+   {/*<CombineHomeNavbar/> */}
+     {/* <CombineFarmers/>   */}
+  {/* <CombineFoods/>  */}
+     {/* <FarmerProfile/>  */}
+     {/*<Profile01/> */}
+    {/* {/<BuyCardAndPayment/>/} */}
+    {/* <FeedBack/> */}
+    {/* <Massage/> */}
+    {/* <genraete/> */}
+    {/* <BuyCardAndPayment2/> */}
+    {/* <BuyCardAndPayment3/> */}
+    {/* <BuyCardAndPayment4/> */}
+    {/* <BuyCardAndPayment5/> */}
+    {/* <BuyCardAndPayment6/> */}
+    {/* <BuyCardAndPayment7/> */}
+    {/* <BuyCardAndPayment8/> */}
+    {/* <BuyCardAndPayment9/> */}
+  </React.StrictMode>
 );
