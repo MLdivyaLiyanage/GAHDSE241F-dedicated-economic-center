a4bea268ffabb129141070531da3bb1f74ad35f6
sadun kumara <dilmiravihansa2002.1.14@gmail.com>
2025-04-10 17:44:04 +0530

    login page

--- economic_center_mobileapp/lib/main.dart
+++ economic_center_mobileapp/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:economic_center_mobileapp/pages/signup.dart';
+import 'package:economic_center_mobileapp/pages/signin.dart';
+//import 'package:economic_center_mobileapp/pages/signup.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SignUp()
+      home: const LoginPage()
     );
   }
 }
